286502ca8207e51439ba19ce34e47c3033e6a781
izzypop123 <146539306+izzypop123@users.noreply.github.com>
2023-10-28 17:29:35 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+const navbar =document.querySelector('navbar')
+const navbaricon =document.querySelector('navbar i')
+const dropdownmenu =document.querySelector('dropdown_menu')
+
+navbar.onclick = function(){
+ dropdownmenu.classList.navbar('open')
+ const isopen = 
+
+
+}
